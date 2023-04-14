2cd7fd65d50aafdd6f7767f9024f1e209a64d867
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-04-14 22:35:34 +0500

    The commit i'm going to checkout

--- App.js
+++ App.js
@@ -1,11 +1,118 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SectionList, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from "react";
+
+// import CustomButton from './CustomButton';
+import CustomButton from './CustomButton';
+
+import axios from 'axios'
+
+const DATA = [
+  {
+    title: 'Main dishes',
+    data: ['Pizza', 'Burger', 'Risotto'],
+  },
+  {
+    title: 'Sides',
+    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  },
+  {
+    title: 'Drinks',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: 'Desserts',
+    data: ['Cheese Cake', 'Ice Cream'],
+  },
+];
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+
+  // useEffect(()=>{
+  //   axios.get('https://fakestoreapi.com/products', {
+  //     // params: {
+  //     //     articleID: articleID
+  //     // }
+  // })
+  //     .then(function (response) {
+          
+  //         console.log('response',response.data);
+          
+  //         setData(response.data)
+      
+  //       })
+  //     .catch(function (error) {
+  //         // console.log(error);
+  //     })
+  //     .then(function () {
+  //         // always executed
+  //         console.log('Always',data[0]);
+  //     });  
+  // },[])
+
+  const getAPIData = async () =>{
+    const url = "https://fakestoreapi.com/products"
+    let result = await fetch(url);
+    result = await result.jason();
+    console.wlogarn(result);
+  }
+  useEffect(()=>{
+    getAPIData();
+  },[])
+
+
+  loginBtnPressed=()=>{
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* <CustomButton afzal='top' />
+      <CustomButton afzal='bottom' />
+      <CustomButton afzal='left' />
+      <CustomButton afzal='right' /> */}
+     
+
+      <FlatList
+        data={data}
+        renderItem={({item}) => 
+          (
+            <View style={{flex:1}}>
+               <View style={styles.view1}>
+                  <Text> {item.title} </Text>
+               </View>
+               <View style={styles.view2}>
+                  <Text> {item.category} </Text>
+              </View>
+
+              
+              <Text> {item.category} </Text>
+            </View>
+          )
+        }
+        keyExtractor={item => item.id}
+      />
+
+
+  {/* <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({item}) => (
+        <View style={{backgroundColor:'green'}}>
+          <Text style={styles.title}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({section: {title}}) => (
+        <View style={{backgroundColor:'lightgrey'}}>
+        <Text style={styles.header}>{title}</Text>
+        </View>
+      )}
+    /> */}
+
     </View>
   );
 }
@@ -14,7 +121,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop:100
   },
+  view1:{
+    flex:0.50, 
+    backgroundColor:'lightgrey'
+  },
+  view2:{
+    flex:0.50, backgroundColor:'lightgrey'
+  }
 });
